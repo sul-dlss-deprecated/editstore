f040075a50efde668c426603d9ed1a084abb8703
Peter Mangiafico <peter@mangiafico.org>
2013-08-06 17:11:20 -0700

    set dependencies on models

--- app/models/editstore/project.rb
+++ app/models/editstore/project.rb
@@ -1,7 +1,7 @@
 module Editstore
   class Project < Connection
-     has_many :changes
-     has_many :fields
+     has_many :changes, :dependent => :destroy
+     has_many :fields, :dependent => :destroy
      attr_accessible :name, :template
      validates :name, presence: true
      validates :template, presence: true
--- lib/editstore/version.rb
+++ lib/editstore/version.rb
@@ -1,3 +1,3 @@
 module Editstore
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
