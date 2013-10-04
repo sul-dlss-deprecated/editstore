193c8336bfd35b752bd6ecb3f60721f3e65e49a9
Peter Mangiafico <peter@mangiafico.org>
2013-10-04 15:55:51 -0700

    update migration

--- db/migrate/20131004224346_add_pending_to_change.rb
+++ db/migrate/20131004224346_add_pending_to_change.rb
@@ -2,7 +2,7 @@ class AddPendingToChange < ActiveRecord::Migration
   def change
     if Editstore.run_migrations?
       @connection=Editstore::Connection.connection
-      add_column :editstore_changes, :pending, :boolean
+      add_column :editstore_changes, :pending, :boolean, :default=>false
     end
   end
 end
