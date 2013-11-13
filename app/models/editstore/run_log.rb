f6b0a00707e874d1e0ebc90a0afa8860ff92a8e0
Peter Mangiafico <peter@mangiafico.org>
2013-11-13 14:48:52 -0800

    add prune of crashed runs from runlogs

--- app/models/editstore/run_log.rb
+++ app/models/editstore/run_log.rb
@@ -8,6 +8,7 @@ module Editstore
       Editstore::RunLog.where('ended IS NOT NULL').order('ended DESC').limit(1).first
     end
     def self.prune
+      self.where('updated_at < ?',1.day.ago).where('ended IS NULL').each {|obj| obj.destroy} # anything older than 1 day that is not marked as finished running (but must be crashed and not actually running)
       self.where('updated_at < ?',1.month.ago).each {|obj| obj.destroy} # anything older than 1 month
       self.where(:total_druids=>0).where('updated_at < ?',1.day.ago).each {|obj| obj.destroy} # anything older than 1 day with no activity
     end
