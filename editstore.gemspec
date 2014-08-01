474dcf3b5682d1ee5aaa02a80c7451ae67c916e6
Chris Beer <cabeer@stanford.edu>
2014-08-01 13:06:38 -0700

    Update to rspec 3

--- editstore.gemspec
+++ editstore.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "sqlite3"
 
-  s.add_development_dependency "rspec"
+  s.add_development_dependency "rspec", "~> 3.0"
   s.add_development_dependency "rspec-rails"
   
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,16 +1,16 @@
 require "spec_helper"
 
-describe Editstore::State do
+describe Editstore::State, :type => :model do
   
   it "should have the correct six states, from cached values" do
     
-    Editstore::State.wait.should == Editstore::State.find_by_name('wait')
-    Editstore::State.ready.should == Editstore::State.find_by_name('ready')
-    Editstore::State.in_process.should == Editstore::State.find_by_name('in process')
-    Editstore::State.applied.should == Editstore::State.find_by_name('applied')
-    Editstore::State.complete.should == Editstore::State.find_by_name('complete')
-    Editstore::State.error.should == Editstore::State.find_by_name('error')
-    Editstore::State.count.should == 6
+    expect(Editstore::State.wait).to eq(Editstore::State.find_by_name('wait'))
+    expect(Editstore::State.ready).to eq(Editstore::State.find_by_name('ready'))
+    expect(Editstore::State.in_process).to eq(Editstore::State.find_by_name('in process'))
+    expect(Editstore::State.applied).to eq(Editstore::State.find_by_name('applied'))
+    expect(Editstore::State.complete).to eq(Editstore::State.find_by_name('complete'))
+    expect(Editstore::State.error).to eq(Editstore::State.find_by_name('error'))
+    expect(Editstore::State.count).to eq(6)
     
   end
   
