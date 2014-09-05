0b75d064dc039d292948bdb2dcc7cbc017dd4331
Carrick Rogers <carrickr@stanford.edu>
2014-09-05 16:38:31 -0700

    Updating to remove one last attr

--- app/models/editstore/field.rb
+++ app/models/editstore/field.rb
@@ -1,6 +1,9 @@
 module Editstore
   class Field < Connection
     belongs_to :project
-    attr_accessible :project_id,:name
+    #attr_accessible :project_id,:name
+    def field_params
+         params.require(:field).permit(:project_id,:name)
+    end
   end
 end
