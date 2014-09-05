c99eb81613bcafe32a7deaa807d27d81c88531dc
Carrick Rogers <carrickr@stanford.edu>
2014-09-05 16:16:37 -0700

    Updating to pass a symbol for connection.

--- app/models/editstore/connection.rb
+++ app/models/editstore/connection.rb
@@ -1,6 +1,6 @@
 module Editstore
   class Connection < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection "editstore_#{Rails.env}"
+    establish_connection "editstore_#{Rails.env}".parameterize.underscore.to_sym
   end
 end
